f9841095e4bdec60a3084de71aacfd884061801e
PixelCoder1246 <sanchitkhajuria905@gmail.com>
2025-03-14 22:06:36 +0530

    Delete backend directory

--- backend/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { verifyTurnstile } = require("../middlewares/recaptchaMiddleware");
-const loginController = require("../controllers/loginController");
-
-const router = express.Router();
-
-router.post("/", verifyTurnstile, loginController.login);
-
-module.exports = { router };
