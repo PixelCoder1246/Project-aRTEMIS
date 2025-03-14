fb5e6c9f2487e31f332ab7e317abdce4cab4f846
PixelCoder1246 <sanchitkhajuria905@gmail.com>
2025-03-14 21:57:28 +0530

    Delete backend directory

--- backend/routes/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { validateSignup } = require('../middlewares/signupMiddleware');
-const signupController = require('../controllers/signupController');
-
-router.post('/', validateSignup, signupController.signup);
-module.exports = { router };
